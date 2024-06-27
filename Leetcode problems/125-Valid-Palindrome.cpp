3b6fd0db46ab1699405f9953b0b0aa755c967dee
Amr Khaled <146705810+amrkld@users.noreply.github.com>
2024-06-27 12:44:37 +0300

    [125] [Time Beats: 100.00%] [Memory Beats: 19.33%] - LeetPush

--- Leetcode problems/125-Valid-Palindrome.cpp
+++ Leetcode problems/125-Valid-Palindrome.cpp
@@ -2,29 +2,19 @@ class Solution {
 public:
     bool isPalindrome(string s) {
         // alphanumeric contains numbers
-        string lowers = \\;
-        for (auto ch : s) {
-            lowers += tolower(ch);
-        }
-
         string alphaS = \\;
-        // Alph is in range from (97 (a) to 122 (z))
-        for (auto ch : lowers) {
-            int chh = int(ch);
-            if ((chh > 96 && chh < 123) || (chh >= 48 && chh <= 57)) {
-                alphaS += ch;
+          for (auto ch : s) {
+            if (std::isalnum(ch)) {
+                alphaS += std::tolower(ch);
             }
         }
 
-        if (alphaS.size() == 1 || alphaS.size() == 0) {
-            return true;
-        }
 
         // two pointers , start & end
         int start = 0;
         int end = alphaS.size() - 1;
 
-        while (start <= int(end + 1 / 2)) {
+        while (start <= end) {
             if (alphaS[start] == alphaS[end]) {
                 start++;
                 end--;
